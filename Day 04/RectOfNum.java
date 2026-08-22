4f9b191b5ad4d273165d16d0199c5e61523d64fc
Pooja Patkar <68493624+Pooja-Patkar-Cosmo@users.noreply.github.com>
2026-08-22 18:43:09 +0530

    Add files via upload

---
+++ Day 04/RectOfNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RectOfNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i=0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++)
+                System.out.print(i+1);
+            System.out.println();
+        }
+        sc.close();   
+    }
+}
